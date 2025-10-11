e7b2dfa8b385797f966357c72e15d84d81fb6029
Gabrielle Freitas <gabih.sante@gmail.com>
2025-10-11 00:30:14 -0300

    cursor

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -54,7 +54,6 @@ function getStarType(index: number): string {
     background-color: #fff;
     box-shadow: 0 2px 8px rgba(0, 0, 0, 0.1);
     border-radius: 3px;
-    cursor: pointer;
 
     &:hover {
       box-shadow: 0 4px 12px rgba(0, 0, 0, 0.15);
