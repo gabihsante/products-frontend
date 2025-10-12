cedcb6ac03584b65040eece8029fd595d43ebcda
Gabrielle Freitas <gabih.sante@gmail.com>
2025-10-12 15:49:31 -0300

    test - favoritar produto

--- src/__tests__/ProductsList.spec.ts
+++ src/__tests__/ProductsList.spec.ts
@@ -2,6 +2,8 @@ import { describe, it, expect, vi } from 'vitest'
 import ProductsList from '@pages/ProductsList.vue'
 import { flushPromises, mount } from '@vue/test-utils'
 
+const mockAddToWishlist = vi.fn()
+
 vi.mock('@services/getProducts.ts', () => ({
   getProducts: vi.fn().mockResolvedValue([
     {
@@ -22,6 +24,10 @@ vi.mock('@services/getProducts.ts', () => ({
   ]),
 }))
 
+vi.mock('@services/wishlist.ts', () => ({
+  addToWishlist: mockAddToWishlist
+}))
+
 describe('products list', () => {
   it('exibe os produtos na tela', async () => {
     const wrapper = mount(ProductsList)
@@ -29,5 +35,20 @@ describe('products list', () => {
     await flushPromises()
 
     expect(wrapper.text()).toContain('Tênis Nike Revolution 7 Feminino - Preto+Branco')
+  })
+})
+
+describe('ProductsList', () => {
+  it('salva o produto na wishlist ao clicar no botão de favorito', async () => {
+    const wrapper = mount(ProductsList)
+
+    await flushPromises()
+
+    const favoriteButton = wrapper.find('products-list__favorite-btn')
+    expect(favoriteButton.exists()).toBe(true)
+
+    await favoriteButton.trigger('click')
+
+    expect(mockAddToWishlist).toHaveBeenCalledExactlyOnceWith('123')
   })
 })
