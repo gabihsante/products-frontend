9383bec6eb6b004c04a776960d1ca3ce20959dd9
Gabrielle Freitas <gabih.sante@gmail.com>
2025-10-12 18:01:15 -0300

    função de favoritar produto

--- src/pages/ProductsList.vue
+++ src/pages/ProductsList.vue
@@ -3,7 +3,11 @@
     <template #product>
       <product-card v-for="product in products" :key="product.code" :product="product">
         <template #action>
-          <button class="products-list__favorite-btn" aria-label="Favoritar produto">
+          <button
+            class="products-list__favorite-btn"
+            aria-label="Favoritar produto"
+            @click="addToWishlist(product.code)"
+          >
             <span class="material-symbols-outlined products-list__favorite-icon"> favorite </span>
           </button>
         </template>
@@ -18,12 +22,34 @@ import ProductCard from '@components/ProductCard.vue'
 import { onMounted, ref } from 'vue'
 import type { Product } from '../types/product.ts'
 import { getProducts } from '@services/getProducts.ts'
+import { setWishlist } from '@services/setWishlist.ts'
+import Swal from 'sweetalert2'
 
 const products = ref<Product[]>([])
 
 onMounted(() => {
   getProducts().then((response) => (products.value = response))
 })
+
+async function addToWishlist(code: string) {
+  await setWishlist(code)
+    .then(() => {
+      Swal.fire({
+        title: 'Successo!',
+        text: 'Produto adicionado aos favoritos',
+        icon: 'success',
+        timer: 1000,
+      })
+    })
+    .catch((err) => {
+      return Swal.fire({
+        title: 'Erro!',
+        text: err,
+        icon: 'error',
+        timer: 1000,
+      })
+    })
+}
 </script>
 
 <style scoped lang="scss">
