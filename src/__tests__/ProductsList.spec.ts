f7992d6593ff4c43eb1c5c9cce286dcf60d3407e
Gabrielle Freitas <gabih.sante@gmail.com>
2025-10-12 14:26:46 -0300

    teste listagem de produtos

--- src/__tests__/ProductsList.spec.ts
+++ src/__tests__/ProductsList.spec.ts
@@ -1,11 +1,33 @@
-import { describe, it, expect } from 'vitest'
+import { describe, it, expect, vi } from 'vitest'
 import ProductsList from '@pages/ProductsList.vue'
-import { mount } from '@vue/test-utils'
+import { flushPromises, mount } from '@vue/test-utils'
+
+vi.mock('@services/getProducts.ts', () => ({
+  getProducts: vi.fn().mockResolvedValue([
+    {
+      code: '123',
+      name: 'Tênis Nike Revolution 7 Feminino - Preto+Branco',
+      available: true,
+      visible: true,
+      details: {
+        name: 'Tênis Nike Revolution 7 Feminino - Preto+Branco',
+        description: 'Teste',
+      },
+      priceInCents: 59999,
+      salePriceInCents: 39999,
+      rating: 4.5,
+      image: 'test',
+      stockAvailable: true,
+    },
+  ]),
+}))
 
 describe('products list', () => {
   it('exibe os produtos na tela', async () => {
     const wrapper = mount(ProductsList)
 
-    expect(wrapper.text()).toBe('Tênis Nike Revolution 7 Feminino - Preto+Branco')
+    await flushPromises()
+
+    expect(wrapper.text()).toContain('Tênis Nike Revolution 7 Feminino - Preto+Branco')
   })
 })
