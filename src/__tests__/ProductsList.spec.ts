c32e973b978cdfeb4cd58290b6d349c1860d0dbd
Gabrielle Freitas <gabih.sante@gmail.com>
2025-10-12 20:18:03 -0300

    atualização no teste

--- src/__tests__/ProductsList.spec.ts
+++ src/__tests__/ProductsList.spec.ts
@@ -1,9 +1,34 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect, vi, beforeEach } from 'vitest'
 import ProductsList from '@pages/ProductsList.vue'
 import { flushPromises, mount } from '@vue/test-utils'
+import { createPinia, setActivePinia } from 'pinia'
+
+beforeEach(() => {
+  setActivePinia(createPinia())
+})
 
 vi.mock('@services/getProducts.ts', () => ({
   getProducts: vi.fn().mockResolvedValue([
+    {
+      code: '123',
+      name: 'Tênis Nike Revolution 7 Feminino - Preto+Branco',
+      available: true,
+      visible: true,
+      details: {
+        name: 'Tênis Nike Revolution 7 Feminino - Preto+Branco',
+        description: 'Teste',
+      },
+      priceInCents: 59999,
+      salePriceInCents: 39999,
+      rating: 4.5,
+      image: 'test',
+      stockAvailable: true,
+    },
+  ]),
+}))
+
+vi.mock('@services/getWishlist.ts', () => ({
+  getWishlist: vi.fn().mockResolvedValue([
     {
       code: '123',
       name: 'Tênis Nike Revolution 7 Feminino - Preto+Branco',
